422ca09c8c4e0ca3b06518143544598ccc594019
calvarado2004 <carlos-alvarado@outlook.com>
2023-04-09 15:47:22 -0400

    function HelloWorld

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './AppClass';
+import AppClass from './AppClass';
+import HelloWorld from "./HelloWorld";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppClass />
+    <HelloWorld />
   </React.StrictMode>
 );
 
